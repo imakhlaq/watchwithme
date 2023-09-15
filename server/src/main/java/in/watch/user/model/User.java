57e38f0dadeb31eb88e6d551608ed746901604e6
imakhlaq <imakhlaqXD@gmail.com>
2023-09-15 14:30:25 +0530

    first commit

---
+++ server/src/main/java/in/watch/user/model/User.java
@@ -0,0 +1,26 @@
+package in.watch.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+
+}
---
+++ server/src/test/java/in/watch/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
